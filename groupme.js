775cc711f7076c402d022f7023a36a15d99c0fa8
connorwiniarczyk <connorwiniarczyk@gmail.com>
2019-02-18 03:22:50 +0000

    Moved Hazing Neil feature to live

--- groupme.js
+++ groupme.js
@@ -1,8 +1,8 @@
 const fetch = require('node-fetch')
 
 const url = "https://api.groupme.com/v3/bots/post"
-// const bot_id = "6956995d8f9cab3bdc9c386da6"
-const bot_id = "cf40fd03f3f763c5d794c97114"
+const bot_id = "6956995d8f9cab3bdc9c386da6" // WJRH-2019 chat
+// const bot_id = "cf40fd03f3f763c5d794c97114" // test room
 
 exports.post = function(message){
 	return fetch(url, {
